3dcc13b22b92f8600de4f462ab1479e050ff09e5
NIOUWAN <beldjoudi2006@yahoo.fr>
2023-09-08 15:22:47 +0200

    test

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+
+
+<nav class="site-navigation">
+
+    /*<!--  liens de menu ici -->*/
+
+</nav>
+</div>
+
+  
+
+<body>
+    <h1 class="Titre">BOISSONS ENERGITIQUE 100% NATURELLE</h1>
+</body>
+
+
+
+
+
